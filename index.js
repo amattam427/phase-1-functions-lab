31d965dc5afe4f937f0c83431cfe79a580087398
Ashley Mattam <amattam427@gmail.com>
2021-11-23 13:14:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const hq= 42;
+function distanceFromHqInBlocks(pickUp) {
+    let distanceB= pickUp-hq;
+    return Math.abs(distanceB);
+}
+  function distanceFromHqInFeet(pickUp){
+    distanceFromHqInBlocks(pickUp);
+    const distanceF= distanceFromHqInBlocks(pickUp)*264;
+    return distanceF;
+  }
+  function distanceTravelledInFeet(start, end){
+     const travel= (end-start)*264;
+      return Math.abs(travel);
+  }
+  function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    const feet = distanceTravelledInFeet(start, destination);
+
+    if (feet <= 400) {
+        return 0;
+    } else if (feet > 400 && feet <=2000) {
+        return (feet - 400) * .02;
+    } else if (feet > 2500) {
+        return 'cannot travel that far';
+    } else if (feet > 2000) {
+        return 25;
+    } 
+
+  }
